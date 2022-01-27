03a4790040814228f52aba4f1f8818ac9ce3e84b
jainbhavuk <bhavukspeaks@gmail.com>
2022-01-27 23:53:47 +0530

    second commit new pages

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const myKey = "y-4J5utYMU4WT8VlQB9-54P_JVL6pZ0t7DFbKFg9bzU";
 let inputImage = document.getElementById('inputImage');
 let imageName="";
-
+let totalPages="";
 inputImage.addEventListener('keydown',(key)=>{
      if(key.key == 'Enter')
      {
@@ -13,19 +13,39 @@ inputImage.addEventListener('keydown',(key)=>{
 
 function getImages()
 {
+    imageName = inputImage.value;
  axios.get(`https://api.unsplash.com/search/photos?client_id=${myKey}&\page=1&query=${imageName}`).then((res)=>{
      console.log(res);
+     totalPages = res.data.total_pages;
+     loadImagesToUI(res.data.results, totalPages);
+     })
+}
+function getNewImages(pageNo)
+{
+ axios.get(`https://api.unsplash.com/search/photos?client_id=${myKey}&\page=${pageNo}&query=${imageName}`).then((res)=>{
+     console.log(res);
+     
      loadImagesToUI(res.data.results);
      })
 }
-
 const loadImagesToUI = (results)=>{
+
     for(let i=0; i<results.length; i++)
     {
         let newDiv = document.createElement('div');
         newDiv.className="img";
-        newDiv.style.backgroundImage="url(" + results[i].urls.regular +")"
+        newDiv.style.backgroundImage="url(" + results[i].urls.regular +")";
+        newDiv.addEventListener('click',()=>{
+            window.open(results[i].links.download, "_blank");
+        })
         console.log(newDiv);
         document.getElementById('grid').appendChild(newDiv);
     }
+    document.getElementById('nextPageBtn').style.display="block";
+}
+let pno=2;
+function newPage(){
+    document.getElementById('nextPageBtn').style.display="none";
+getNewImages(pno);
+pno++;
 }
